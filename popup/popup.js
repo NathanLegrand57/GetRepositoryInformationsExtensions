50f9e6d7543b2f9bd770095fbb7d4d682b9e8a0c
Nathan <nathanlgd14@gmail.com>
2025-03-19 21:53:55 +0100

    Added style and improved retrieve url function

--- popup/popup.js
+++ popup/popup.js
@@ -1,12 +1,26 @@
-chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    let activeTab = tabs[0];
-    let url = activeTab.url;
-    console.log("URL de l'onglet actif :", url);
-});
+function getUrl() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        const urlElement = document.getElementById("url");
+        const url = tabs[0].url;
+        alert(url);
+        urlElement.textContent = url;
+        urlElement.href = url;
+        let lastPart = url;
+        let website = lastPart.split('/')[2];
+        if (website = "github.com") {
+            console.log("You're on github");
+            getRepository(url);
+        } else {
+            alert("You're not on github website");
+        }
+    });
+}
 
 document.addEventListener("DOMContentLoaded", function () {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        document.getElementById("url").textContent = tabs[0].url;
-        alert(tabs[0].url);
-    });
+    getUrl();
 });
+
+function getRepository(website) {
+    let repository = website.split('/')[3];
+    alert("Site : " + repository);
+}
