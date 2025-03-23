94c1cae69709bf494d2392a733184f8a5312d0e2
Nathan <nathanlgd14@gmail.com>
2025-03-23 15:05:01 +0100

    Added fetch function to retrieve api informations about current repository

--- popup/popup.js
+++ popup/popup.js
@@ -1,26 +1,57 @@
 function getUrl() {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        const urlElement = document.getElementById("url");
-        const url = tabs[0].url;
-        alert(url);
-        urlElement.textContent = url;
-        urlElement.href = url;
-        let lastPart = url;
-        let website = lastPart.split('/')[2];
-        if (website = "github.com") {
-            console.log("You're on github");
-            getRepository(url);
-        } else {
-            alert("You're not on github website");
-        }
-    });
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    const urlElement = document.getElementById("url");
+    const url = tabs[0].url;
+    // alert(url);
+    urlElement.textContent = url;
+    urlElement.href = url;
+    const lastPart = url;
+    const website = lastPart.split("/")[2];
+    if (website === "github.com") {
+      // alert("You're on github");
+      getRepository(url);
+    } else {
+      alert("You're not on github website");
+    }
+  });
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    getUrl();
+  getUrl();
 });
 
 function getRepository(website) {
-    let repository = website.split('/')[3];
-    alert("Site : " + repository);
+  const autorName = website.split("/")[3];
+  const repositoryName = website.split("/")[4];
+  if (repositoryName === undefined) {
+    alert("Repository not found");
+  } else {
+    fetchData(autorName, repositoryName);
+    alert("Repository: " + repositoryName);
+  }
+}
+
+async function fetchData(autorName, repositoryName) {
+  const url = `https://api.github.com/repos/${autorName}/${repositoryName}`;
+  let data;
+  try {
+    alert("gngng :" + url);
+    const response = await fetch(url);
+    data = await response.json();
+  } catch (e) {
+    alert("Error", e);
+  }
+
+  const filteredValues = {
+    id: data.id,
+    name: data.name,
+    login: data.owner.login,
+    created_at: data.created_at,
+    visibility : data.private,
+  };
+
+  console.log(
+    `This is the informations about the repository ${repositoryName} :`,
+    filteredValues
+  );
 }
